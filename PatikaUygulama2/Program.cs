66cebea23573209e3edb662fb4c7c9f9e1737b08
Aysel Bilmez <aybilmez@gmail.com>
2024-10-03 17:49:15 +0300

    Proje dosyası ekle.

---
+++ PatikaUygulama2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace PatikaUygulama2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lutfen bir sayi giriniz: ");
+            int Sayi = Convert.ToInt32 (Console.ReadLine()); //gelen string degeri tam sayiya cevirir
+
+            if (Sayi >= 0) //girilen sayinin pozitifligini kontrol ediyorum 
+            {
+
+                if (Sayi > 10) // sayinin 10'dan buyuk olup olmadigi sorgulaniyor
+                {
+                    Console.WriteLine("Girilen sayi 10'dan buyuktur.");
+                }
+                else if (Sayi == 10) // sayi 10'a esitse girilecek if statement
+                {
+                    Console.WriteLine("Girilen sayi 10'dur.");
+                }
+                else // bu else statement eger sayi hem pozitif hem de 10 ve ustu bir sayi degilse
+                {
+                    Console.WriteLine("Sayi 10'dan kucuktur.");
+                }
+
+                if (Sayi % 2 == 1) // sayinin 2 tabanindaki modunu alir
+                {
+                    Console.WriteLine("Sayi tektir.");
+                }
+                else // sayinin 2 tababnindaki modu sifir ise bu statementa girer (Tek sayı)
+                {
+                    Console.WriteLine("Girilen sayi cifttir.");
+                }
+            }
+            else // sayi sifirdan kucukse tekrar denenmesi icin hata belirtilmesidir
+            {
+                Console.WriteLine("Girilen sayi negatiftir lutfen pozitif bir sayi girip tekrar deneyiniz.");
+            }
+        }
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,48 @@
+
+# C# Patika Uygulama 2 - If/Else Statement 
+
+## Genel Bakış 
+
+Bu C# konsol aplikasyonu karşı taraftan bir tam sayı isteyerek aşağıdaki işlemleri kontrol ediyor:
+1. Bu sayı pozitif mi?
+2. Bu sayı 10'dan küçük mü, büyük mü, yoksa eşit mi?
+3. Bu sayı pozitif mi yoksa negatif mi? 
+
+## Özellikler
+
+- Bu program girilen sayının negatif olma durumuna bakmaktadır.
+- Sayıya bakarak 10 sayısına göre olan durumunu söylemektedir. 
+- Ayrıca program sayının çiftlik durumuna da bakıp feedback vermektedir. 
+
+## Nasıl Çalıştırılır?
+
+1. Programı çalıştırınız.
+2. İstenildiğinde tam sayıyı giriniz.
+3. Program şunları gösterecektir:
+   - Negatif bir sayı olup olmadığı,
+   - 10 sayısından büyük, eşit, veya küçük olup olmadığını,
+   - Çift bir sayı olup olmadığını.
+
+## Gereklilikler
+
+- .NET SDK (Version 5.0 veya daha üst versionlar)
+- C# derleyici
+
+## Nasıl Derlenir?
+
+Programı derlemek için terminalde aşağıdaki kodu çalıştırın:
+```bash
+csc Program.cs
+```
+
+## Örnek Çıktı
+
+```
+Lutfen bir sayi giriniz: 12
+Girilen sayi 10'dan buyuktur.
+Girilen sayi cifttir.
+```
+
+## Lisans
+
+Bu proje kullanılmaya ve modifiye edilmeye açıktır.
